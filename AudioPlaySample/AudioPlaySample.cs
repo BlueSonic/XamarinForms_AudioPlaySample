1405198520b9aeac94f415f4212ac47a44f07256
BlueSonic <admin@ADVANCE-160008.local>
2016-08-18 18:58:31 +0900

    upload

---
+++ AudioPlaySample/AudioPlaySample.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AudioPlaySample
+{
+	public class App : Application
+	{
+		public interface IMediaPlayer
+		{
+			Task PlayAsync(string title);
+			void Stop();
+			void GetStatus();
+		}
+
+		public App()
+		{
+			Button btnPlay = new Button {
+				Text = "Play/Pause",
+				Command = new Command((obj) => {
+					DependencyService.Get<IMediaPlayer>().PlayAsync("shall_we_meet");
+				})
+			};
+
+			Button btnStop = new Button {
+				Text = "Stop",
+				Command = new Command((obj) => {
+					DependencyService.Get<IMediaPlayer>().Stop();
+				})
+			};
+
+			Button btnInfo = new Button {
+				Text = "GetStatus",
+				Command = new Command((obj) => {
+					DependencyService.Get<IMediaPlayer>().GetStatus();
+				})
+			};
+
+			// The root page of your application
+			var content = new ContentPage {
+				Title = "AudioPlaySample",
+				Content = new StackLayout {
+					VerticalOptions = LayoutOptions.Center,
+					Children = {
+						btnPlay,
+						btnStop,
+						btnInfo
+					}
+				},
+				Padding = new Thickness(10)
+			};
+			MainPage = new NavigationPage(content);
+		}
+
+		protected override void OnStart()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
+
---
+++ Droid/MediaPlayer_Droid.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Android.Media;
+
+[assembly: Dependency(typeof(AudioPlaySample.Droid.MediaPlayer_Droid))]
+namespace AudioPlaySample.Droid
+{
+	public class MediaPlayer_Droid : App.IMediaPlayer
+	{
+		MediaPlayer player = null;
+
+		private async Task StartPlayerAsync(string title)
+		{
+			var resourceId = (int)typeof(Resource.Raw).GetField(title).GetValue(null);
+
+			await Task.Run(() => {
+				if (player == null) {
+					player = new MediaPlayer();
+					player.SetAudioStreamType(Stream.Music);
+
+					player = MediaPlayer.Create(
+								global::Android.App.Application.Context,
+								resourceId
+							);
+					player.Looping = true;
+					player.Start();
+				} else {
+					if (player.IsPlaying == true) {
+						player.Pause();
+					} else {
+						player.Start();
+					}
+				}
+			});
+		}
+
+		private void StopPlayer()
+		{
+			if ((player != null)) {
+				if (player.IsPlaying) {
+					player.Stop();
+				}
+				player.Release();
+				player = null;
+			}
+		}
+
+		public async Task PlayAsync(string title)
+		{
+			await StartPlayerAsync(title);
+		}
+
+		public void Stop()
+		{
+			StopPlayer();
+		}
+
+		public void GetStatus()
+		{
+			if ((player != null)) {
+				if (player.IsPlaying) {
+					int total = player.Duration;
+					int now = player.CurrentPosition;
+					string msg = "PlayNow / Total :" + Environment.NewLine
+					              + TimeSpan.FromMilliseconds(now) + " / " + TimeSpan.FromMilliseconds(total);
+
+					Console.WriteLine(msg);
+					App.Current.MainPage.DisplayAlert("GetStatus", msg, "OK");
+				} else {
+					App.Current.MainPage.DisplayAlert("GetStatus", "Paused", "OK");
+				}
+			} else {
+				App.Current.MainPage.DisplayAlert("GetStatus", "Stopped", "OK");
+			}
+		}
+	}
+}
